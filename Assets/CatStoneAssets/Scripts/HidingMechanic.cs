aa15fac29502f0aa8264dfbd41881e28be406765
CzarT1 <114026758+CzarT1@users.noreply.github.com>
2024-11-02 21:31:40 -0400

    more changes

--- Assets/CatStoneAssets/Scripts/HidingMechanic.cs
+++ Assets/CatStoneAssets/Scripts/HidingMechanic.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HidingMechanic : MonoBehaviour
 {
     public GameObject hideText, stopHideText;
-     public GameObject normalPlayer, hidingPlayer;
+    public GameObject normalPlayer, hidingPlayer;
     public BasicAhhEnemyAI monsterScript;
     public Transform monsterTransform;
     bool interactable, hiding;
     public float loseDistance;
+    
+    //GameManager Object, useful for pulling specific scripts and objects for use in other scripts. Automatically set in "Start()".
+    private GameObject gameManagerinstance;
+
+//Gets the Input Actions Asset to draw inputs from.
+    //Drop the action map "XRI RightHand Interaction"
+    [SerializeField]
+    [Tooltip("Drag and drop the Input Actions Asset that this script would use.")]
+    public InputActionProperty XRIInputActionsAsset;
+
 
     void Start()
     {
         interactable = false;
         hiding = false;
+        //Instantializes the game manager object.
+        gameManagerinstance = GameObject.Find("GameManagerObject");
     }
     void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("MainCamera"))
+        if (other.gameObject.tag == "Player" && hiding == false)
         {
-            hideText.SetActive(true);
+            other.gameObject.transform.position = this.gameObject.transform.position;
+            hiding = true;
             interactable = true;
         }
     }
@@ -28,40 +42,43 @@ public class HidingMechanic : MonoBehaviour
     {
         if (other.CompareTag("MainCamera"))
         {
-            hideText.SetActive(false);
+            //hideText.SetActive(false);
             interactable = false;
         }
     }
     void Update()
     {
-        if(interactable == true)
+        if (interactable == true)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (XRIInputActionsAsset.action.WasPressedThisFrame())
             {
-                hideText.SetActive(false);
-                hidingPlayer.SetActive(true);
+                //hideText.SetActive(false);
+                //hidingPlayer.SetActive(true);
+                Debug.Log("LEMME OUT");
                 float distance = Vector3.Distance(monsterTransform.position, normalPlayer.transform.position);
-                if(distance > loseDistance)
+                if (distance > loseDistance)
                 {
-                    if(monsterScript.chasing == true)
+                    if (monsterScript.chasing == true)
                     {
                         monsterScript.stopChase();
                     }
                 }
-                stopHideText.SetActive(true);
+                //stopHideText.SetActive(true);
                 hiding = true;
-                normalPlayer.SetActive(false);
+                //normalPlayer.SetActive(false);
                 interactable = false;
             }
         }
-        if(hiding == true)
+        if (hiding == true)
         {
-            if (Input.GetKeyDown(KeyCode.Q))
+            if (XRIInputActionsAsset.action.WasPressedThisFrame())
             {
-                stopHideText.SetActive(false);
-                normalPlayer.SetActive(true);
-                hidingPlayer.SetActive(false);
+                //stopHideText.SetActive(false);
+                //normalPlayer.SetActive(true);
+                //hidingPlayer.SetActive(false);
                 hiding = false;
+                gameManagerinstance.GetComponent<GameManagerScript>().playerObject.transform.position = new Vector3(0, 0, 0);
+
             }
         }
     }
